e9f613c234ffc81bb447baad5449fd4d4942b370
Ondřej Hošek <ondra.hosek@gmail.com>
2021-03-25 07:51:17 +0100

    add unit tests for enumerate

--- src/cmds/enumerate.rs
+++ src/cmds/enumerate.rs
@@ -79,3 +79,112 @@ pub fn enumerate(args: &[String]) -> i32 {
 
     ret
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::addr::{Ipv4Address, Ipv6Address};
+    use crate::net::IpNetwork;
+
+    fn parse_ipv4(s: &str) -> Ipv4Address { s.parse().unwrap() }
+    fn parse_ipv6(s: &str) -> Ipv6Address { s.parse().unwrap() }
+    fn parse_ipv4net(addr_str: &str, cidr: usize) -> IpNetwork<Ipv4Address> {
+        IpNetwork::new_with_prefix(addr_str.parse().unwrap(), cidr)
+    }
+    fn parse_ipv4netm(addr_str: &str, mask_str: &str) -> IpNetwork<Ipv4Address> {
+        IpNetwork::new_with_mask(addr_str.parse().unwrap(), mask_str.parse().unwrap())
+    }
+    fn parse_ipv6net(addr_str: &str, cidr: usize) -> IpNetwork<Ipv6Address> {
+        IpNetwork::new_with_prefix(addr_str.parse().unwrap(), cidr)
+    }
+    fn parse_ipv6netm(addr_str: &str, mask_str: &str) -> IpNetwork<Ipv6Address> {
+        IpNetwork::new_with_mask(addr_str.parse().unwrap(), mask_str.parse().unwrap())
+    }
+
+    #[test]
+    fn test_enumerate_subnet() {
+        let mut iter = NetworkIter::new(parse_ipv4net("192.0.2.64", 28));
+        assert_eq!(Some(parse_ipv4("192.0.2.64")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.65")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.66")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.67")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.68")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.69")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.70")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.71")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.72")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.73")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.74")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.75")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.76")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.77")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.78")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.0.2.79")), iter.next());
+        assert_eq!(None, iter.next());
+        assert_eq!(None, iter.next());
+
+        let mut iter = NetworkIter::new(parse_ipv6net("2001:db8::10", 124));
+        assert_eq!(Some(parse_ipv6("2001:db8::10")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::11")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::12")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::13")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::14")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::15")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::16")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::17")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::18")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::19")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1a")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1b")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1c")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1d")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1e")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1f")), iter.next());
+        assert_eq!(None, iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn test_enumerate_mixed_subnet() {
+        let mut iter = NetworkIter::new(parse_ipv4netm("192.64.2.0", "255.240.255.255"));
+        assert_eq!(Some(parse_ipv4("192.64.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.65.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.66.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.67.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.68.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.69.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.70.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.71.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.72.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.73.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.74.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.75.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.76.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.77.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.78.2.0")), iter.next());
+        assert_eq!(Some(parse_ipv4("192.79.2.0")), iter.next());
+        assert_eq!(None, iter.next());
+        assert_eq!(None, iter.next());
+
+        let mut iter = NetworkIter::new(parse_ipv6netm("2001:db8::1042", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:f0ff"));
+        assert_eq!(Some(parse_ipv6("2001:db8::1042")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1142")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1242")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1342")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1442")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1542")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1642")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1742")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1842")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1942")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1a42")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1b42")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1c42")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1d42")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1e42")), iter.next());
+        assert_eq!(Some(parse_ipv6("2001:db8::1f42")), iter.next());
+        assert_eq!(None, iter.next());
+        assert_eq!(None, iter.next());
+    }
+}
