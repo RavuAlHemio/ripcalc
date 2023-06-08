9f29e882b00a2ffc606353618c796e0a04f91c29
Ondřej Hošek <ondra.hosek@gmail.com>
2023-06-08 23:12:26 +0200

    optimize release builds

--- build-wasm.ps1
+++ build-wasm.ps1
@@ -1,2 +1,3 @@
 & cargo build --package=wasmripcalc --target=wasm32-unknown-unknown --release
 Copy-Item -Path .\target\wasm32-unknown-unknown\release\wasmripcalc.wasm .\wasmripcalc\tsglue\wasmripcalc.wasm
+Write-Output (Get-Item .\wasmripcalc\tsglue\wasmripcalc.wasm).Length
